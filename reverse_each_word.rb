79f0aac16e4a0a67d261f0519f203797708af6a1
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-10 16:27:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word (sentence)
+  final_sentence = ""
   sentence_a = sentence.split(/ /)
   sentence_a.collect do |word|
-    word.reverse
+    final_sentence << word.reverse
   end
 end
