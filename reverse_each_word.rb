33a91a9c36b92cfcb504eeb70a9895021039d918
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-10 16:26:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 def reverse_each_word (sentence)
   sentence_a = sentence.split(/ /)
   sentence_a.collect do |word|
-    if word == sentence_a.last
-      word.reverse
-    else
-      word.reverse
-      word << " "
+    word.reverse
     end
   end
 end
